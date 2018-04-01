afb1b2015fb92fe5e8f342b649f2cd2f80b4e978
Enis Gusinac <gusinacenis88@gmail.com>
2018-04-01 12:04:46 +0200

    Image insertion in Blog

--- site/snippets/coverimage.php
+++ site/snippets/coverimage.php
@@ -1,5 +1,5 @@
 <?php if($image = $item->coverimage()->toFile()): ?>
   <figure>
-    <img src="<?= $image->url() ?>" alt="" />
+    <img src="<?= $image->url() ?>" alt="featured image"/>
   </figure>
 <?php endif ?>
