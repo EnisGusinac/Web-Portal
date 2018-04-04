f7b64edfec3d79ff09ed3517a7f9b13d694e4d57
Enis Gusinac <gusinacenis88@gmail.com>
2018-04-04 22:14:06 +0200

    Added footer and removed unused double code

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -37,20 +37,4 @@
     <?php snippet('menu') ?>
 
 
-<!-- jquery Core-->
-<script src="assets/js/jquery-2.1.4.min.js"></script>
-
-<!-- Bootstrap -->
-<script src="assets/js/bootstrap.min.js"></script>
-
-<!-- Theme Menu -->
-<script src="assets/js/mobile-menu.js"></script>
-
-<!-- Owl carousel -->
-<script src="assets/js/owl.carousel.min.js"></script>
-
-<!-- Theme Script -->
-<script src="assets/js/script.js"></script>
-</body>
-</html>
 
